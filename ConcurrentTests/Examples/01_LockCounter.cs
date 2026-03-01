f8bd38c999aa8588a8fcef3751140db2eb66901c
UglukFearless <UglukFearless@mail.ru>
2026-03-02 02:07:22 +0700

    Add first example

---
+++ ConcurrentTests/Examples/01_LockCounter.cs
@@ -0,0 +1,55 @@
+﻿
+namespace ConcurrentTests.Examples;
+
+internal class _01_LockCounter
+{
+    private int _unsafeCounter = 0;
+    private int _lockCounter = 0;
+
+    private object _lock = new();
+
+    public async Task DoAction()
+    {
+        try
+        {
+            await DoUnsafe();
+            await DoSafe();    
+        }
+        catch (Exception ex) 
+        { 
+            Console.Error.WriteLine(ex.ToString()); // First failed task exception
+        }
+        
+    }
+
+    private async Task DoUnsafe()
+    {
+        Console.WriteLine("Unsafe counter (without lock): starting...");
+        var unsafeTasks = Enumerable.Range(0, 10).Select(_ => Task.Run(() =>
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                _unsafeCounter++;
+            }
+        }));
+        await Task.WhenAll(unsafeTasks);
+        Console.WriteLine($"Result: {_unsafeCounter}");
+    }
+
+    private async Task DoSafe()
+    {
+        Console.WriteLine("Safe counter (with lock): starting...");
+        var safeTasks = Enumerable.Range(0, 10).Select(_ => Task.Run(() =>
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                lock (_lock)
+                {
+                    _lockCounter++;
+                }
+            }
+        }));
+        await Task.WhenAll(safeTasks);
+        Console.WriteLine($"Result: {_lockCounter}");
+    }
+}
---
+++ ConcurrentTests/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConcurrentTests.Examples;
+
+Console.WriteLine("Concurrent programming examples:");
+Console.WriteLine();
+
+_01_LockCounter example01 = new();
+await example01.DoAction();
+Console.WriteLine();
+
+Console.WriteLine("Done.");
